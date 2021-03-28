1d854ed6cea9cdf9ba08f09da57e9f0fd3661d17
Alejandro G. Fernández de Castro <a.gonzalez.fdez@gmail.com>
2021-03-28 11:09:07 +0200

    Add files via upload

--- rhamnus.R
+++ rhamnus.R
@@ -10,12 +10,15 @@ canarias<-spTransform(canarias, projectiongeo)
 #2.RHAMNUS ####
 
 rhamnus<-read_delim("Rhamnus.csv",delim=";") %>% group_by(SPECIES) %>% 
-  arrange(y,by_group=TRUE)
+  arrange(y,by_group=TRUE) %>% 
+  filter(y>1)
 species<-levels(factor(rhamnus$SPECIES))
-ggplot(canarias,aes(long,lat,group=group,fill="white",col="black"))+
+ggplot(canarias,aes(long,lat,group=group))+
   geom_polygon()+
   geom_point(data=rhamnus,aes(x,y,group=NULL))+
-  theme_bw()
+  theme_bw()+ theme(panel.background = element_rect(fill = "lightblue",
+                                    colour = "lightblue",
+                                    size = 0.5, linetype = "solid")
 
 for(s in species){
   especies<-filter(rhamnus,SPECIES==s)
