01a7ed15610219345eac8684470e7432f094ba38
Alejandro G. Fernández de Castro <a.gonzalez.fdez@gmail.com>
2021-03-30 08:58:32 +0200

    Add files via upload

--- rhamnus.R
+++ rhamnus.R
@@ -1,11 +1,15 @@
 #0.PREPARACIÓN####
-packages<-c("tidyverse","biomod2","ENMTools","sp","rgdal")
+packages<-c("tidyverse","biomod2","ENMTools","sp","rgdal",
+            "raster","ggfortify","FactoMineR","ggfortify")
 sapply(packages,require,character.only=TRUE)
 
 projectionUTM<-"+proj=utm +zone=28 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
 projectiongeo<-"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+
+setwd("~/rhamnus")
+
 canarias<-readOGR("islas.shp") 
-canarias<-spTransform(canarias, projectiongeo)
+canariasgeo<-spTransform(canarias, projectiongeo)
 
 presence.absence.raster <- function (mask.raster,species.data,raster.label="") {
   require(raster)
@@ -22,47 +26,75 @@ presence.absence.raster <- function (mask.raster,species.data,raster.label="") {
   return(speciesRaster)
 }
 
-
+ggplot(canariasgeo,aes(long,lat,group=group))+geom_polygon()+
+  geom_point(data=rhamnus,aes(x,y,group=NULL,col="red"))
 #1.DATOS CLIMÁTICOS####
 
 setwd("~/Spalmensis")
 
-bio01<-crop(raster("bio01canarias.asc"),template)%>%mask(mask)
-bio02<-crop(raster("bio02canarias.asc"),template)%>%mask(mask)
-bio03<-crop(raster("bio03canarias.asc"),template)%>%mask(mask)
-bio04<-crop(raster("bio04canarias.asc"),template)%>%mask(mask)
-bio05<-crop(raster("bio05canarias.asc"),template)%>%mask(mask)
-bio06<-crop(raster("bio06canarias.asc"),template)%>%mask(mask)
-bio07<-crop(raster("bio07canarias.asc"),template)%>%mask(mask)
-bio08<-crop(raster("bio08canarias.asc"),template)%>%mask(mask)
-bio09<-crop(raster("bio09canarias.asc"),template)%>%mask(mask)
-bio10<-crop(raster("bio10canarias.asc"),template)%>%mask(mask)
-bio11<-crop(raster("bio11canarias.asc"),template)%>%mask(mask)
-bio12<-crop(raster("bio12canarias.asc"),template)%>%mask(mask)
-bio13<-crop(raster("bio13canarias.asc"),template)%>%mask(mask)
-bio14<-crop(raster("bio14canarias.asc"),template)%>%mask(mask)
-bio15<-crop(raster("bio15canarias.asc"),template)%>%mask(mask)
-bio16<-crop(raster("bio16canarias.asc"),template)%>%mask(mask)
-bio17<-crop(raster("bio17canarias.asc"),template)%>%mask(mask)
-bio18<-crop(raster("bio18canarias.asc"),template)%>%mask(mask)
-bio19<-crop(raster("bio19canarias.asc"),template)%>%mask(mask)
+bio01<-raster("bio01canarias.asc")%>%mask(canarias)
+bio02<-raster("bio02canarias.asc")%>%mask(canarias)
+bio03<-raster("bio03canarias.asc")%>%mask(canarias)
+bio04<-raster("bio04canarias.asc")%>%mask(canarias)
+#bio05<-raster("bio05canarias.asc")%>%mask(canarias)
+#bio06<-raster("bio06canarias.asc")%>%mask(canarias)
+bio07<-raster("bio07canarias.asc")%>%mask(canarias)
+bio08<-raster("bio08canarias.asc")%>%mask(canarias)
+bio09<-raster("bio09canarias.asc")%>%mask(canarias)
+#bio10<-raster("bio10canarias.asc")%>%mask(canarias)
+#bio11<-raster("bio11canarias.asc")%>%mask(canarias)
+bio12<-raster("bio12canarias.asc")%>%mask(canarias)
+#bio13<-raster("bio13canarias.asc")%>%mask(canarias)
+#bio14<-raster("bio14canarias.asc")%>%mask(canarias)
+bio15<-raster("bio15canarias.asc")%>%mask(canarias)
+#bio16<-raster("bio16canarias.asc")%>%mask(canarias)
+#bio17<-raster("bio17canarias.asc")%>%mask(canarias)
+bio18<-raster("bio18canarias.asc")%>%mask(canarias)
+bio19<-raster("bio19canarias.asc")%>%mask(canarias)
 
-tpi<-raster("tpicanarias.asc")%>%crop(template)%>%mask(mask)
-slope<-raster("slopecanarias.asc")%>%crop(template)%>%mask(mask)
+tpi<-raster("tpicanarias.asc")%>%mask(canarias)
+slope<-raster("slopecanarias.asc")%>%mask(canarias)
 
-variables<-stack(bio01,bio02,bio03,bio04,bio05,bio06,bio07,bio08,bio09,bio10,
-                 bio11,bio12,bio13,bio14,bio15,bio16,bio17,bio18,bio19,tpi,slope)
+variables<-stack(bio01,bio02,bio03,bio04,bio07,bio08,bio09,bio12,bio15,bio18,bio19,tpi,slope)
 
-data<-raster::extract(x=variables,y=xy.sambucus,layer=1, nl=21,na.rm=TRUE)%>%data.frame()
-data<-cbind(data,origen)%>%na.omit()
-colnames(data)<-c("bio01","bio02","bio03","bio04","bio05","bio06","bio07","bio08","bio09","bio10","bio11","bio12","bio13","bio14",
-                  "bio15","bio16","bio17","bio18","bio19","tpi","slope","origen")
-df<-data[1:21]
+
+#2.RHAMNUS####
+
+rhamnus<-read_delim("Rhamnus.csv",delim=";") %>% 
+  #group_by(SPECIES) %>% 
+  #arrange(y,by_group=TRUE) %>% 
+  filter(y>1)
+species<-levels(factor(rhamnus$SPECIES))
+ggplot(canarias,aes(long,lat,group=group))+
+  geom_polygon()+
+  geom_point(data=rhamnus,aes(x,y,group=NULL))+
+  theme_bw()+ theme(panel.background = element_rect(fill = "lightblue",
+                                    colour = "lightblue",
+                                    size = 0.5, linetype = "solid"))
+coordinates(rhamnus)<-~x+y
+
+proj4string(rhamnus)<-projectiongeo
+rhamnusUTM<-spTransform(rhamnus,projectionUTM) %>% as.data.frame()
+
+data<-raster::extract(x=variables,y=rhamnusUTM[c("x","y")],layer=1, nl=21,na.rm=TRUE)%>%data.frame()
+data<-cbind(data,rhamnus$SPECIES)%>%na.omit()
+
+colnames(data)[ncol(data)]<-"species"
+df<-data[1:ncol(data)-1]
 
 autoplot(prcomp(df),data=data,
-         colour="origen",
+         colour="species",
          loadings.label = TRUE, loadings.label.size = 3)
 
+
+pca<-PCA(data[1:ncol(data)-1],graph=F)
+
+fviz_pca_ind(pca,
+             label = "none", # hide individual labels
+             habillage = as.factor(data$species), # color by groups
+             palette = c("#00AFBB", "#E7B800"),
+             addEllipses = TRUE # Concentration ellipses
+)
 sambucus.natural<-subset(sambucus,Origen=="Natural")
 head(sambucus.natural)
 xy.sambucus.natural<-cbind(sambucus.natural$x,sambucus.natural$y)
@@ -88,20 +120,6 @@ hier.data$bio07<-NULL
 hier.data$bio19<-NULL
 hier.data$tpi<-NULL
 hier<-hier.part(hier.data[,length(hier.data)],hier.data[1:length(hier.data)-1],barplot=TRUE)
-
-
-#2.RHAMNUS####
-
-rhamnus<-read_delim("Rhamnus.csv",delim=";") %>% group_by(SPECIES) %>% 
-  arrange(y,by_group=TRUE) %>% 
-  filter(y>1)
-species<-levels(factor(rhamnus$SPECIES))
-ggplot(canarias,aes(long,lat,group=group))+
-  geom_polygon()+
-  geom_point(data=rhamnus,aes(x,y,group=NULL))+
-  theme_bw()+ theme(panel.background = element_rect(fill = "lightblue",
-                                    colour = "lightblue",
-                                    size = 0.5, linetype = "solid"))
 
 for(s in species){
   especies<-filter(rhamnus,SPECIES==s)
